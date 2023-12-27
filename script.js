35934a0e9ecfc290a70345c84a1cf0820414cb63
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-27 15:10:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ function firstNonRepeatedChar(str) {
  let n=str.length;
 	
 
-	let nonREPEAT="",chech=true;
-for(let i=0;i<n;i=i+2){
-	if(str[i]!=str[i+1]){
-		return str[i];
+	let nonREPEAT="",check={};
+	
+for(let i=0;i<n;i=i+1){
+	if(Object.hasOwn(check,str[i])){
+	check[str[i]]++;
+	}
+	else{
+		check[str[i]]=1;
 	}
 }
+	
+	for(let i in check){
+		if(check[i]==1){
+			return i;
+		}
+	}
+	
 return null;
 	}
 const input = prompt("Enter a string");
