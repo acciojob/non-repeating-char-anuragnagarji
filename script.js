b79f5248cbeef97d3211f9b9be6a06200deb9f0a
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-27 13:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function firstNonRepeatedChar(str) {
  let n=str.length;
-	if(n%2==0)
- {
-	 return null;
- }
-else{
+	
+
 	let nonREPEAT="",chech=true;
 for(let i=0;i<n;i=i+2){
 	if(str[i]!=str[i+1]){
 		return str[i];
 	}
 }
-}
-	
- 
-}
+return null;
+	}
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
